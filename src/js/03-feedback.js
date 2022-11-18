6ba067dbd0980168f7be1ac466b5014063b62099
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-18 22:06:32 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const STORAGE_KEY = 'feedback-form-state';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log({})
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
