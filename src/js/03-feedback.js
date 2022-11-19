255e0c3262ef48040f8ac5480804f1f6dcb634e4
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-19 18:51:57 +0200

    task 3 fn

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,23 +6,39 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const refs = {
     form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
     textarea: document.querySelector('.feedback-form textarea'),
 };
 
+// const user = {
+//     email: refs.input.value,
+//     message: refs.textarea.value,
+// };
+
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+// refs.input.addEventListener('input', throttle(onTextareaInput, 500));
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextarea();
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    console.log({
+        email: refs.input.value,
+        message: refs.textarea.value,
+    });
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextareaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
+    const user = {
+        email: refs.input.value,
+        message: refs.textarea.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(user));
+    
 }
 
 function populateTextarea() {
